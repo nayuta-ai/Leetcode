8c83138a97d8f30807bdbeca32cc478d0bcda511
nsyuta-ai <nayuta723@gmail.com>
2021-08-09 12:28:29 +0900

    0809

--- 122.py
+++ 122.py
@@ -2,12 +2,14 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         ans = 0
         sta = 100000
-        for i in range(len(prices)-1):
+        for i in range(len(prices) - 1):
             if prices[i] < prices[i+1]:
-                sta = min(prices[i],sta)
+                sta = min(prices[i], sta)
             if sta != 100000 and prices[i] > prices[i+1]:
-                ans += prices[i]-sta
+                ans += prices[i] - sta
                 sta = 100000
         if sta != 100000:
-            ans += prices[len(prices)-1]-sta
+            ans += prices[len(prices) - 1] - sta
         return ans
+# Runtime 64ms
+# Memory 15.2MB
