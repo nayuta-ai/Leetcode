7708b359aa10de6486ef4c6e21269dd8c6e95b3d
nayuta-ai <nayuta723@gmail.com>
2021-08-26 11:04:09 +0900

    first commit 168

---
+++ 168_Excel_Sheet_Column_Title.py
@@ -0,0 +1,13 @@
+# time O(n) n:number of loop
+# space O(m) alphabet list:26, out:26^m
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        alphabet = [chr(i) for i in range(65,65+26)]
+        out = ''
+        while columnNumber > 0:
+            remainder = columnNumber%26 - 1
+            out = alphabet[remainder] + out
+            columnNumber = columnNumber//26
+            if remainder == -1:
+                columnNumber -= 1
+        return out
