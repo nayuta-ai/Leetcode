33f175cfc15c6dcc9e4193cf747f4ce5c9482dc8
nayuta-ai <nayuta723@gmail.com>
2021-08-31 19:10:56 +0900

    first commit 172

---
+++ 172_Factorial_Trailing_Zeroes.py
@@ -0,0 +1,10 @@
+# time O(1)
+# space O(1)
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        cnt = 0
+        for i in range(1,6):
+            if n < 5**i:
+                break;
+            cnt = cnt + n//(5**i)
+        return cnt
