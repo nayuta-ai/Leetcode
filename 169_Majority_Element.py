8135b13b7f4a205d8d70e4a50191217539b1bbe4
nayuta-ai <nayuta723@gmail.com>
2021-09-01 09:33:21 +0900

    Counter

--- 169_Majority_Element.py
+++ 169_Majority_Element.py
@@ -1,7 +1,7 @@
 # space O(n)
 # time O(n)
-import collections
+from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        c = collections.Counter(nums)
+        c = Counter(nums)
         return c.most_common()[0][0]
