b5d3d6c3cd9c86ba9eed7eaa4b8ab3172ba3c66b
nayuta-ai <nayuta723@gmail.com>
2021-09-01 09:02:36 +0900

    add space complexity and Insert a blank line after return statement

--- 145_Binary_Tree_Postorder_Traversal.py
+++ 145_Binary_Tree_Postorder_Traversal.py
@@ -1,9 +1,11 @@
-# O(n)
+# time O(n)
+# space O(n)
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         def loop(root):
             if root is None:
                 return 
+                
             ans.append(root.val)
             loop(root.right)
             loop(root.left)
