67bcb2e4931fcc59e57c8af5b3f2f2ada736ab90
nayuta-ai <nayuta723@gmail.com>
2021-09-01 09:36:12 +0900

    insert space and a block line

--- 172_Factorial_Trailing_Zeroes.py
+++ 172_Factorial_Trailing_Zeroes.py
@@ -4,7 +4,8 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         cnt = 0
         for i in range(1,6):
-            if n < 5**i:
+            if n < 5 ** i:
                 break;
-            cnt = cnt + n//(5**i)
+                
+            cnt = cnt + n // (5 ** i)
         return cnt
