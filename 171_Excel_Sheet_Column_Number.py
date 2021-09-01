b7348f517ddfbe6ba6b5632c1328616c48ff8911
nayuta-ai <nayuta723@gmail.com>
2021-09-01 09:46:17 +0900

    avoid magic number

--- 171_Excel_Sheet_Column_Number.py
+++ 171_Excel_Sheet_Column_Number.py
@@ -1,8 +1,9 @@
 # time O(n) n:len(columnTitle)
 # space O(m) m:length of alphabet list
+import string
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
-        alphabet = [chr(i) for i in range(65,65+26)]
+        alphabet = list(string.ascii_uppercase)
         length = len(columnTitle)
         ans = 0
         for i in columnTitle:
