0873d598c2ba1b322fb8fd7003da888ead7fa681
nayuta-ai <nayuta723@gmail.com>
2021-10-05 11:39:40 +0900

    add alphabet_num

--- 168_Excel_Sheet_Column_Title.py
+++ 168_Excel_Sheet_Column_Title.py
@@ -5,10 +5,11 @@ class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
         alphabet = list(string.ascii_uppercase)
         out = ''
+        alphabet_num = 26
         while columnNumber > 0:
-            remainder = columnNumber % 26 - 1
+            remainder = columnNumber % alphabet_num - 1
             out = alphabet[remainder] + out
-            columnNumber = columnNumber // 26
+            columnNumber = columnNumber // alphabet_num
             if remainder == -1:
                 columnNumber -= 1
         return out
