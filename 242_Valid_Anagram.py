2a09d10b43557e3330f42928e1403f8fa9bdfdaa
nayuta-ai <nayuta723@gmail.com>
2021-10-10 11:16:10 +0900

    add 242

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,17 @@
+# time complexity: O(1)
+# space complexity: O(n): n is length of dict
+import collections
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        Args:
+            s: str: given strings, 1 <= s.length <= 5*10**4
+            t: str: given strings, 1 <= s.length <= 5*10**4
+        Returns:
+            bool: s and t is anagram
+        """
+        assert len(s) != 0, "length of s is one or more"
+        assert len(t) != 0, "length of t is one or more"
+        s_dic = collections.Counter(s)
+        t_dic = collections.Counter(t)
+        return s_dic == t_dic
